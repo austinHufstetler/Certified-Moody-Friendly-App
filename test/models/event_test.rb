40691d8930e29b583d7855269c6292d5bc908800
Austin Hufstetler <austinhufstetler@gmail.com>
2018-03-11 15:33:01 -0400

    edited tests, edited css of home page

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "event attributes must not be empty" do
+		event = Event.new
+		assert event.invalid?
+		assert event.errors[:title].any?
+		assert event.errors[:description].any?
+		assert event.errors[:image_url].any?
+		assert event.errors[:start_date].any?
+		assert event.errors[:end_date].any?
+	end
+
 end
