84143305c2e42bbd1c5c36a2c58243e4e6af0360
John Bedene <bedenejohn@gmail.com>
2018-04-04 10:00:06 -0400

    Google Map integration hopefully done

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,8 +3,6 @@ class Business < ApplicationRecord
 	has_one :account, as: :accountable
 	geocoded_by :address
   	after_validation :geocode
-  	reverse_geocoded_by :latitude, :longitude
-	after_validation :reverse_geocode
 	has_many :coupons,dependent: :destroy
 	has_many :events,dependent: :destroy
 	
