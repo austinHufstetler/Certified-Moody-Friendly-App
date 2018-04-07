bffa7642d6a21f7c8f7cf03b9af036f5f448b0ad
Austin Hufstetler <austinhufstetler@gmail.com>
2018-04-06 21:19:49 -0400

    chamber member snow have admin permissions!

---
+++ app/dashboards/business_dashboard.rb
@@ -0,0 +1,75 @@
+require "administrate/base_dashboard"
+
+class BusinessDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    account: Field::HasOne,
+    coupons: Field::HasMany,
+    events: Field::HasMany,
+    id: Field::Number,
+    name: Field::String,
+    address: Field::String,
+    logo_url: Field::String,
+    description: Field::Text,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    latitude: Field::Number.with_options(decimals: 2),
+    longitude: Field::Number.with_options(decimals: 2),
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :account,
+    :coupons,
+    :events,
+    :id,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :account,
+    :coupons,
+    :events,
+    :id,
+    :name,
+    :address,
+    :logo_url,
+    :description,
+    :created_at,
+    :updated_at,
+    :latitude,
+    :longitude,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :account,
+    :coupons,
+    :events,
+    :name,
+    :address,
+    :logo_url,
+    :description,
+    :latitude,
+    :longitude,
+  ].freeze
+
+  # Overwrite this method to customize how businesses are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(business)
+  #   "Business ##{business.id}"
+  # end
+end
