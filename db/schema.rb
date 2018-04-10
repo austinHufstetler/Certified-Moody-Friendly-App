5bc6c2d7aeaa481431f1d2cb386bae2149684e1f
Faisal Shuraym <faisal.sh13@gmail.com>
2018-04-10 13:30:24 -0400

    pulling the new changes from master.

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,6 @@ ActiveRecord::Schema.define(version: 20180407193438) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "chambers", force: :cascade do |t|
