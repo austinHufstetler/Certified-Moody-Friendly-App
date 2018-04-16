e4e81e824fe49aad41423e49e8d6bf41b7e2c26d
John <bedenejohn@gmail.com>
2018-04-16 14:13:21 -0400

    added migrations

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20180414034225) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "chambers", force: :cascade do |t|
