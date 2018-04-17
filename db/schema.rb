0120b53ec2d9a7cb8a464f7c5c9daa7c2c9e01bd
Austin Hufstetler <austinhufstetler@gmail.com>
2018-04-16 23:19:12 -0400

    minor css changes and added address to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414034225) do
+ActiveRecord::Schema.define(version: 20180417021541) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20180414034225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "business_id"
+    t.string "address"
     t.index ["business_id"], name: "index_events_on_business_id"
   end
 
